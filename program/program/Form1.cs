eff1747455ce63a0b9995925def2d7a0c6659466
Administrator <Administrator@WPO-06754>
2016-06-24 17:32:02 +0200

    3

--- program/program/Form1.cs
+++ program/program/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace program
 {
@@ -17,6 +18,10 @@ namespace program
             InitializeComponent();
         }
 
+        List<Huffman> lista = new List<Huffman>();  // Node List.
+        FileStream potok = null;
+        String plik = null;
+
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog okno = new OpenFileDialog();
@@ -26,7 +31,17 @@ namespace program
             {
                 try
                 {
+                    plik = okno.FileName;
+                    potok = new FileStream(plik, FileMode.Open, FileAccess.Read);
 
+                    for (int i = 0; i < potok.Length; i++)
+                    {
+                        string odczytany = Convert.ToChar(potok.ReadByte()).ToString();
+                        if (lista.Exists(x => x.znak == odczytany)) // Checking the value that have you created before?
+                            lista[lista.FindIndex(y => y.znak == odczytany)].zwiekszCzestotliwosc(); // If is yes, find the index of the Node and increase the frequency of the Node.
+                        else
+                            lista.Add(new Huffman(odczytany));   // If is no, create a new node and add to the List of Nodes
+                    }
                 }
                 catch (Exception ex)
                 {
---
+++ program/program/Huffman.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Huffman : IComparable<Huffman>
+    {
+        public string znak;   // For the character of char wartosc. Public because Process class use it.
+        public int czestotliwosc;          // Number of the count on file, string, text.
+        public string kod;            // Getting from a tree for making a huffman tree.
+        public Huffman rodzic; // Parent Node of current Node.
+        public Huffman lewy;   // Left Node of current Node.
+        public Huffman prawy;  // Right Node of current Node.
+        public bool lisc;            // Shows it is a leaf.
+
+
+        public Huffman(string wartosc)    // Creating a Node with given wartosc(character).
+        {
+            znak = wartosc;     // Setting the znak.
+            czestotliwosc = 1;      // This is creation of Node, so now its count is 1.
+
+            prawy = lewy = rodzic = null;       // Does not have a left or right tree and a parent.
+
+            kod = "";          // It will be Assigned on the making Tree. Now it is empty.
+            lisc = true;      // Because all Node we create first does not have a parent Node.
+        }
+
+
+        public Huffman(Huffman pierwszy, Huffman drugi) // Join the 2 Node to make Node.
+        {
+            // Firsly we are adding this 2 Nodes' variables. Except the new Node's left and right tree.
+            kod = "";
+            lisc = false;
+            rodzic = null;
+
+            // Now the new Node need leaf. They are pierwszy and drugi. if pierwszy's czestotliwosc is bigger than or equal to drugi's czestotliwosc. It is right tree. Otherwise left tree. The controllers are below:
+            if (pierwszy.czestotliwosc >= drugi.czestotliwosc)
+            {
+                prawy = pierwszy;
+                lewy = drugi;
+                prawy.rodzic = lewy.rodzic = this;     // "this" means the new Node!
+                znak = pierwszy.znak + drugi.znak;
+                czestotliwosc = pierwszy.czestotliwosc + drugi.czestotliwosc;
+            }
+            else if (pierwszy.czestotliwosc < drugi.czestotliwosc)
+            {
+                prawy = drugi;
+                lewy = pierwszy;
+                lewy.rodzic = prawy.rodzic = this;     // "this" means the new Node!
+                znak = drugi.znak + pierwszy.znak;
+                czestotliwosc = drugi.czestotliwosc + pierwszy.czestotliwosc;
+            }
+        }
+
+
+        public int CompareTo(Huffman inny) // We just override the CompareTo method. Because when we compare two Node, it must be according to frequencies of the Nodes.
+        {
+            return this.czestotliwosc.CompareTo(inny.czestotliwosc);
+        }
+
+
+        public void zwiekszCzestotliwosc()             // When facing a same wartosc on the Node list, it is increasing the czestotliwosc of the Node.
+        {
+            czestotliwosc++;
+        }
+    }
+}
